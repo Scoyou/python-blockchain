73eec293c6c6840915cf1bafd2c9d026458c114d
Scott Young <scottyoung5@mail.weber.edu>
2018-06-10 05:38:20 -0600

    Define valid_proof, and proof_of_work functions

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
-import requests, datetime, time
+import hashlib, json
+from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
@@ -46,10 +48,44 @@ class Blockchain(object):
         )
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        """Simple Proof of Work algorithm
+
+        -Find a number p such that hash(pp') contains four leading 0's, where p is the previous p'
+        -p is the previous proof, and p' is the new proof
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """Validates the proof. Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous proof
+        :param proof: <int> current proof
+        :return bool:  True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     @staticmethod
     def hash(block):
-        pass
+        """Creates a sha-256 hash of a Block
+
+        :param block: <dick> Block
+        :return: <str>
+        """
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
