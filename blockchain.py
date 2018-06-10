1650a26b3e003d5458d47a55d99c5bbbe63222e1
Scott Young <scottyoung5@mail.weber.edu>
2018-06-10 05:59:19 -0600

    Defined new_transaction function

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
 
@@ -108,7 +108,16 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
+
+    required= ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'missing values', 400
+
+    index = blockchain.new_transaction(values['sender'],values['recipient'],values['amount'])
+
+    response = {'message': f'Transaction will be added to the block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
