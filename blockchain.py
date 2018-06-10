d2b213e7a9c2d5f9a2d43f09911c45866d4d32f0
Scott Young <scottyoung5@mail.weber.edu>
2018-06-10 05:21:14 -0600

    Define new_bock function

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,33 @@
-import requests
+import requests, datetime, time
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
+        self.new_block(previous_hash=1, proof=100)
+
     def new_block(self):
-        pass
+        """Creates a new block in the Blockchain
+
+        :param proof: <int> the proof given by the proof of work algorithm
+        :param previous_hash: <str> hash of previous Block
+        :return: <dict> new block
+        """
+
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = [] #reset list of transactions
+
+        self.chain.append(block)
+        return block
+
 
     def new_transaction(self,sender,recipient,amount):
         """Creates a new transaction that will go into the next mined into the block
@@ -15,7 +36,6 @@ class Blockchain(object):
         :param recipient: <str> address of the recipient
         :param amount: <int> amount
         :return: <int> the index of the block that will hold this transaction
-
         """
         self.current_transactions.append(
             {
